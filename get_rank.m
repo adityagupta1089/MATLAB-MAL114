f64ef439082c1b607948ff51fea64095fc1cdbda
Aditya <adityagupta1089@gmail.com>
2016-02-09 10:25:37 +0530

    RREF itself gives exact entries

--- get_rank.m
+++ get_rank.m
@@ -2,11 +2,10 @@ function rank = get_rank(A)
 A=row_reduced_echleon_form(A);% find the rref
 [r,c]=size(A);% size of A
 rank=0;%initialize rank as 0
-error=1e-10;%error which is required to treat values such as 1e-16 as 0
 for i=1:r
     zerorow=true;% suppose the row is zero all
     for j=1:c
-        if abs(A(i,j))>=error% if we find a non zero row then our assumption is false
+        if abs(A(i,j))~=0% if we find a non zero row then our assumption is false
            zerorow=false;
            break;
         end
--- main.m
+++
@@ -1,6 +0,0 @@
-clc
-clear all
-A=[1 2 1 3 1;2 1 5 -3 1;1 2 1 3 1;3 0 9 -9 1;1 1 2 0 1]; 
-disp(A);
-disp(rref(A));
-disp(get_basis(A,'n'));

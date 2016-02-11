4797fc04e00dd60a1ea95f29110896be8963534b
Aditya <adityagupta1089@gmail.com>
2016-02-11 15:47:51 +0530

    RREF and BASIS

--- get_basis.m
+++ get_basis.m
@@ -1,18 +1,50 @@
 function basis = get_basis(X,type)
 % type r->row, c->column, n->null
 if type=='n'
-   %TODO
+    Y=row_reduced_echleon_form(X);
+    j=1;
+    [r,c]=size(X);
+    Z=eye(c);
+    k=1;
+    free=[];
+    for i=1:r
+        while Y(i,j)==0
+            j=j+1;
+            if(j>c)
+                break;
+            end            
+        end
+        if(j>c)
+                break;
+            end 
+        free(k)=j;
+        k=k+1;
+    end
+    for i=k-1:-1:1
+        Z(:,free(i))=[];
+        Y(:,free(i))=[];
+    end
+    u=1;
+    for i=1:c
+        if ismember(i,free)==1
+            for j=1:c-k+1
+                Z(i,j)=-Y(u,j);
+            end
+            u=u+1;
+        end
+    end
+    basis=Z;
+    return;
 elseif type=='r'
     X=X';
 end
 [r,c]=size(X);
 Y=row_reduced_echleon_form(X);
 j=1;
-tol=1e-10;
 basis=[];%basis set
 k=1;
 for i=1:r
-    while abs(Y(i,j))<tol
+    while Y(i,j)==0
         j=j+1;
         if(j>c)
             break;

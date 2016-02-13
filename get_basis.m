2402a043316cb5453bf283b92b84a36bd20e5e00
2015CSB1003 <2015CSB1003@kickseed.cse.iitp.ac.in>
2016-02-13 12:39:37 +0530

    GET_BASIS

--- get_basis.m
+++ get_basis.m
@@ -2,63 +2,36 @@ function basis = get_basis(X,type)
 % type r->row, c->column, n->null
 if type=='n'
     Y=row_reduced_echleon_form(X);
-    j=1;
     [r,c]=size(X);
-    Z=eye(c);
-    k=1;
+    basis=eye(c);
     free=[];
     for i=1:r
-        while Y(i,j)==0
-            j=j+1;
-            if(j>c)
+        for j=i:c
+            if Y(i,j)~=0
+                free=[free j];
                 break;
-            end            
-        end
-        if(j>c)
-            break;
-        end 
-        free(k)=j;
-        k=k+1;
-    end
-    for i=k-1:-1:1
-        Z(:,free(i))=[];
-        Y(:,free(i))=[];
-    end
-    u=1;
-    for i=1:c
-        if ismember(i,free)==1
-            for j=1:c-k+1
-                Z(i,j)=-Y(u,j);
             end
-            u=u+1;
         end
     end
-    basis=Z;
+    basis(:,free)=[];
+    Y(:,free)=[];
+    basis(free,:)=-Y(1:length(free),:);
     return;
 elseif type=='r'
     X=X';
 end
 [r,c]=size(X);
 Y=row_reduced_echleon_form(X);
-j=1;
-basis=[];%basis set
-k=1;
+basis=[];
 for i=1:r
-    while Y(i,j)==0
-        j=j+1;
-        if(j>c)
-            break;
+    for j=i:c
+        if Y(i,j)~=0
+            basis=[basis X(:,i)];
         end
     end
-    if (j<=c)
-        basis(:,k)=X(:,j);
-        k=k+1;
-        j=j+1;
-    end
-    if j>=c
-        if type=='r'
-            basis=basis';
-        end
-        return;
-    end
+end
+if type=='r'
+    basis=basis';
+end
+return;
 end
